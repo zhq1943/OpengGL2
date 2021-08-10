14b598287e485d81c9cbe667ba45015937361548
zhq1943 <engineer1943@163.com>
2021-08-11 00:45:20 +0800

    Use EBO draw rectangle

--- OpenglLearn/src/main.cpp
+++ OpenglLearn/src/main.cpp
@@ -16,9 +16,15 @@ void processInput(GLFWwindow* window)
 }
 
 float vertices[] = {
-	-0.5,-0.5,0.0f,
+	0.5,0.5,0.0f,
 	0.5f,-0.5f,0.0f,
-	0.0f,0.5f,0.0f
+	-0.5f,-0.5f,0.0f,
+	-0.5,0.5,0.0f
+};
+
+unsigned int indices[] = {
+	0,1,3,
+	1,2,3
 };
 
 const char* vertexShaderSource = "#version 330 core\n"
@@ -61,18 +67,19 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//call this function on every window resize
 
 	//vertex array object
-	unsigned int VAO;
+	unsigned int VAO,VBO,EBO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	unsigned int VBO;
+	glGenBuffers(1, &EBO);//element buffer object
 	glGenBuffers(1, &VBO);//create vertex buffer object
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);//bind buffer to gl_array_buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//copy data to buffer
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//copy indics data to buffer
 	glEnableVertexAttribArray(0);
-
-	
 
 	//vertexshader
 	unsigned int vertexShader;
@@ -132,7 +139,9 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);//swap color buffer
 		glfwPollEvents();//check if any events are triggered(keyborad mousemove)
